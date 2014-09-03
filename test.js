4df0dd575edcb3e8bfb83eb7685905c32c8f85fd
Adam Monsen <haircut@gmail.com>
2014-09-03 10:08:28 -0700

    broken counter: triggers infinite loop

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 if (Meteor.isClient) {
-    var items1count = 0;
+    Session.setDefault("items2count", 0);
     Template.main.helpers({
-        // Helper that returns an array literal. Called once per #each.
-        items1: function () {
-            items1count++;
-            console.log("called " + items1count + " time(s)");
+        // Broken: gets called repeatedly!
+        items2: function () {
+            Session.set("items2count", Session.get("items2count") + 1);
+            items2count = Session.get("items2count");
+            console.log("called " + items2count + " time(s)");
             return [{name:'foo'}, {name:'bar'}];
         }
     });
